6357731f74557a5daa314ecc4fc5c29df978a7b4
Alexander Lead <a.lead@yandex.ru>
2024-03-02 17:01:06 +0300

    feat(server): boot_spring

---
+++ src/main/java/com/ait_31_2/doctor_appointment_app/DoctorAppointmentAppApplication.java
@@ -0,0 +1,13 @@
+package com.ait_31_2.doctor_appointment_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorAppointmentAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorAppointmentAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ait_31_2/doctor_appointment_app/DoctorAppointmentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ait_31_2.doctor_appointment_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorAppointmentAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
