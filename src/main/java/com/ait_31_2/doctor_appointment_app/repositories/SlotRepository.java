2832c087b26fd8ba0d36ad651bdfc9f290c8bc00
Tetiana Ilienko <tatyanailyenko@gmail.com>
2024-03-19 08:05:30 +0100

    update slotServis, slotServiceInterface

---
+++ src/main/java/com/ait_31_2/doctor_appointment_app/repositories/SlotRepository.java
@@ -0,0 +1,7 @@
+package com.ait_31_2.doctor_appointment_app.repositories;
+
+import com.ait_31_2.doctor_appointment_app.domain.classes.Slot;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SlotRepository extends JpaRepository<Slot,Integer> {
+}
---
+++ src/main/java/com/ait_31_2/doctor_appointment_app/services/SlotService.java
@@ -0,0 +1,43 @@
+package com.ait_31_2.doctor_appointment_app.services;
+
+import com.ait_31_2.doctor_appointment_app.domain.classes.Slot;
+import com.ait_31_2.doctor_appointment_app.repositories.SlotRepository;
+import com.ait_31_2.doctor_appointment_app.services.interfaces.SlotServiceInterface;
+
+import java.util.List;
+
+public class SlotService implements SlotServiceInterface {
+    private SlotRepository repository;
+
+    public SlotService(SlotRepository repository) {
+        this.repository = repository;
+    }
+
+
+
+
+    @Override
+    public List<Slot> getAllSlots() {
+        return repository.findAll();
+    }
+
+    @Override
+    public Slot getSlotById(int id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Slot createSlot(Slot slot) {
+        return repository.save(slot);
+    }
+
+    @Override
+    public Slot updateSlot(int id, Slot slot) {
+        return null;
+    }
+
+    @Override
+    public void deleteSlot(int id) {
+
+    }
+}
